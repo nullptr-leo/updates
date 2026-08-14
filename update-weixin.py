dcaff2db43daa6e13c88711be46d7b2ef4d1b57e
Leo <leo@nullptr.com.cn>
2026-08-14 08:42:48 +0800

    Support clean old version for wechat

--- update-weixin.py
+++ update-weixin.py
@@ -13,6 +13,24 @@ proxy = None
 # find out the utilities executable path
 weixin_path = updater.find_install_dir(r'Tencent\Weixin')
 
+# remove old version directories
+version_paths = []
+for _name in os.listdir(weixin_path):
+    _full = os.path.join(weixin_path, _name)
+    if not os.path.isdir(_full):
+        continue
+    _m = re.match(r'^(\d+(?:\.\d+)*)$', _name)
+    if _m:
+        try:
+            version_paths.append((updater.version.parse(_name), _full))
+        except Exception:
+            pass
+if len(version_paths) >= 2:
+    version_paths.sort(key=lambda t: t[0])
+    for _, _full in version_paths[:-1]:
+        print('Removing old version directory: %s' % _full)
+        shutil.rmtree(_full, ignore_errors=True)
+
 # query the remote version
 print('Querying...')
 try:
